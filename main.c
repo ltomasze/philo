d7696172263d81be5a29a26e872cce3a5263ec68
Lukasz Tomaszewski <ltomasze@c3r11s4.42warsaw.pl>
2024-11-23 15:59:56 +0100

    start and valid args

--- main.c
+++ main.c
@@ -1,145 +1,68 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/time.h>
+# define PHILO_MAX 200
 
-typedef struct s_philo {
-    int id;
-    long time_to_die;
-    long time_to_eat;
-    long time_to_sleep;
-    long last_meal;
-    pthread_mutex_t *mutex_death;
-    int *is_dead;
-    pthread_mutex_t *mutex_forks;
-    int num_philosophers;
-} t_philo;
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	num;
 
-long get_current_time() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec * 1000 + tv.tv_usec / 1000; // Czas w milisekundach
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\n'
+		|| nptr[i] == '\v' || nptr[i] == '\f' || nptr[i] == '\r')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		num = num * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (num * sign);
 }
 
-void *philosopher_routine(void *arg) {
-    t_philo *philo = (t_philo *)arg;
-
-    if (philo->num_philosophers == 1) {
-        // Filozof umiera, bo nie ma drugiego do dzielenia widelca
-        usleep(philo->time_to_die * 1000); // Czas życia filozofa minął
-        pthread_mutex_lock(philo->mutex_death);
-        if (!(*philo->is_dead)) {
-            *philo->is_dead = 1;
-            printf("Philosopher %d died.\n", philo->id);
-        }
-        pthread_mutex_unlock(philo->mutex_death);
-        return NULL;
-    }
-
-    while (1) {
-        // Sprawdzanie, czy filozof umarł
-        pthread_mutex_lock(philo->mutex_death);
-        if (*philo->is_dead) {
-            pthread_mutex_unlock(philo->mutex_death);
-            break;
-        }
-        pthread_mutex_unlock(philo->mutex_death);
-
-        // Filozof bierze widelce (symulacja)
-        pthread_mutex_lock(&philo->mutex_forks[philo->id - 1]);
-        pthread_mutex_lock(&philo->mutex_forks[philo->id % 5]);
-
-        // Filozof je
-        printf("Philosopher %d is eating.\n", philo->id);
-        philo->last_meal = get_current_time();
-        usleep(philo->time_to_eat * 1000); // Czas jedzenia
-
-        // Filozof odkłada widelce
-        pthread_mutex_unlock(&philo->mutex_forks[philo->id - 1]);
-        pthread_mutex_unlock(&philo->mutex_forks[philo->id % 5]);
-
-        // Sprawdzamy, czy filozof przekroczył czas do śmierci
-        if (get_current_time() - philo->last_meal > philo->time_to_die) {
-            pthread_mutex_lock(philo->mutex_death);
-            if (!(*philo->is_dead)) {
-                *philo->is_dead = 1;
-                printf("Philosopher %d died.\n", philo->id);
-            }
-            pthread_mutex_unlock(philo->mutex_death);
-            break;
-        }
-
-        // Filozof śpi
-        printf("Philosopher %d is sleeping.\n", philo->id);
-        usleep(philo->time_to_sleep * 1000); // Czas spania
-    }
-
-    return NULL;
+int check_inside_arg(char *arg)
+{
+	int i;
+	
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		if (arg[i] < '0' || arg[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int main(int argc, char **argv) {
-    if (argc != 5) {
-        printf("Usage: ./philo <num_philosophers> <time_to_die> <time_to_eat> <time_to_sleep>\n");
-        return 1;
-    }
+int check_correct_args(char **argv)
+{
+	if (ft_atoi(argv[1]) > PHILO_MAX || ft_atoi(argv[1]) <= 0
+		|| check_inside_arg(argv[1]) == 1)
+		return (write(2, "WRONG NUMBER OF PHILOSOPHERS!\n", 30), 1);
+	if (ft_atoi(argv[2]) <= 0 || check_inside_arg(argv[2]) == 1)
+		return (write(2, "WRONG TIME TO DIE PHILOSOPHERS!\n", 32), 1);
+	if (ft_atoi(argv[3]) <= 0 || check_inside_arg(argv[3]) == 1)
+		return (write(2, "WRONG TIME TO EAT PHILOSOPHERS!\n", 32), 1);
+	if (ft_atoi(argv[4]) <= 0 || check_inside_arg(argv[4]) == 1)
+		return (write(2, "WRONG TIME TO SLEEP PHILOSOPHERS!\n", 34)), 1;
+	if (argv[5] && ft_atoi(argv[5]) < 0 || check_inside_arg(argv[5]) == 1)
+		return (write(2, "WRONG NBR OF TIMES EACH PHILO MUST EAT!\n", 43), 1);
+	return (0);
+}
 
-    // Odczytanie argumentów z linii komend
-    int num_philosophers = atoi(argv[1]);  // Liczba filozofów z argv[1]
-    long time_to_die = atol(argv[2]);  // Czas do śmierci z argv[2]
-    long time_to_eat = atol(argv[3]);  // Czas jedzenia z argv[3]
-    long time_to_sleep = atol(argv[4]);  // Czas spania z argv[4]
 
-    // Mutexy i zmienna do śmierci
-    pthread_mutex_t mutex_death = PTHREAD_MUTEX_INITIALIZER;
-    int is_dead = 0;
-
-    // Mutexy dla widełek
-    pthread_mutex_t mutex_forks[num_philosophers];
-    int i = 0;
-    while (i < num_philosophers) {
-        pthread_mutex_init(&mutex_forks[i], NULL);
-        i++;  // Zwiększenie indeksu
-    }
-
-    // Tworzenie wątków dla filozofów
-    pthread_t threads[num_philosophers];
-    t_philo philosophers[num_philosophers];
-
-    i = 0;
-    while (i < num_philosophers) {  // Zmiana z for na while
-        philosophers[i].id = i + 1;
-        philosophers[i].time_to_die = time_to_die;
-        philosophers[i].time_to_eat = time_to_eat;
-        philosophers[i].time_to_sleep = time_to_sleep;
-        philosophers[i].last_meal = get_current_time();
-        philosophers[i].mutex_death = &mutex_death;
-        philosophers[i].is_dead = &is_dead;
-        philosophers[i].mutex_forks = mutex_forks;
-        philosophers[i].num_philosophers = num_philosophers;
-
-        // Tworzenie wątku dla każdego filozofa
-        if (pthread_create(&threads[i], NULL, philosopher_routine, &philosophers[i]) != 0) {
-            perror("Failed to create thread");
-            return 1;
-        }
-
-        i++;  // Zwiększenie indeksu w pętli
-    }
-
-    // Czekamy na zakończenie wszystkich wątków
-    i = 0;
-    while (i < num_philosophers) {  // Zmiana z for na while
-        pthread_join(threads[i], NULL);
-        i++;  // Zwiększenie indeksu
-    }
-
-    // Zwalniamy mutexy dla widełek
-    i = 0;
-    while (i < num_philosophers) {
-        pthread_mutex_destroy(&mutex_forks[i]);
-        i++;  // Zwiększenie indeksu
-    }
-
-    return 0;
+int	main(int argc, char **argv)
+{
+	if (argc != 5 && argc != 6)
+		return(write(2, "WRONG NUMBER OF ARGUMENTS!\n", 23), 1);
+	if (check_correct_args(argv) == 1)
+		return (1);
+	return (0);
 }
