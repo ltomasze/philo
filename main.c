0cbe6aba50ec9bffaa4fe125fff2b602687ef292
Łukasz Tomaszewski <162119685+ltomasze@users.noreply.github.com>
2024-12-01 15:43:37 +0100

    add eat

--- main.c
+++ main.c
@@ -292,7 +292,26 @@ void	*monitor(void	*ptr)
 
 void	eat(t_philo *philo)
 {
-	pthread_mutex
+	pthread_mutex_lock(philo->right_fork);
+	display_info("has taken a fork", philo, philo->id);
+	if (philo->number_of_philosophers == 1)
+	{
+		ft_usleep(philo->time_to_die);
+		pthread_mutex_unlock(philo->right_fork);
+		return;
+	}
+	pthread_mutex_lock(philo->left_fork);
+	display_info("has taken a fork", philo, philo->id);
+	philo->eat_now = 1;
+	display_info("is eating", philo, philo->id);
+	pthread_mutex_lock(philo->meal_lock);
+	philo->last_meal = get_current_time();
+	philo->meals_eaten++;
+	pthread_mutex_unlock(philo->meal_lock);
+	ft_usleep(philo->time_to_eat);
+	philo->eat_now = 0;
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
 }
 
 
